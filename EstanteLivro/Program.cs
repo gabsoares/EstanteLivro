824022500d853668c3aad139e99ebf875f1cdfd8
gabsoares <soaresgabriel68@gmail.com>
2024-04-30 01:18:20 -0300

    Fix - corrigido problema na impressão dos autores, commit final.

--- EstanteLivro/Program.cs
+++ EstanteLivro/Program.cs
@@ -1,6 +1,7 @@
-﻿int opcao = 0;
+﻿char opcao = 'i';
 Livro livro = new Livro();
-Livro[] estante = new Livro[livro.autores.Length];
+Console.WriteLine("Digite a quantidade de livros que quer guardar na estante: ");
+Livro[] estante = new Livro[int.Parse(Console.ReadLine())];
 
 Livro CadastroLivros()
 {
@@ -9,12 +10,12 @@ Livro CadastroLivros()
     Console.Write("Titulo: ");
     l_temp.DefinirTitulo(Console.ReadLine());
 
-    for (int i = 0; i < (livro.autores).Length; i++)
+    for (int i = 0; i < (l_temp.autores).Length; i++)
     {
         Console.Write($"Autor {i + 1}: ");
-        livro.autores[i] = Console.ReadLine();
+        l_temp.autores[i] = Console.ReadLine();
     }
-    l_temp.DefinirAutores(livro.autores);
+    l_temp.DefinirAutores(l_temp.autores);
 
     Console.Write("Data lancamento: ");
     l_temp.DefinirDataLancamento(DateOnly.Parse(Console.ReadLine()));
@@ -41,8 +42,32 @@ for (int i = 0; i < estante.Length; i++)
     estante[i] = CadastroLivros();
 }
 
-for (int i = 0; i < estante.Length; i++)
+while ((opcao != 'i' && opcao != 'I') || (opcao != 'g' && opcao != 'G'))
 {
-    estante[i].ImprimirLivro();
-    Console.WriteLine();
+    Console.WriteLine("Digite I para consultar algum índice\nDigite G para consulta geral");
+    opcao = char.Parse(Console.ReadLine());
+    switch (opcao)
+    {
+        case 'i':
+        case 'I':
+            Console.Write("Digite o índice para consultar o livro: ");
+            Console.WriteLine();
+            estante[int.Parse(Console.ReadLine())].ImprimirLivro();
+            break;
+        case 'g':
+        case 'G':
+            Console.WriteLine("Consulta do índice geral");
+            for (int i = 0; i < estante.Length; i++)
+            {
+                estante[i].ImprimirLivro();
+                Console.WriteLine();
+            }
+            break;
+        default:
+            Console.Write("Digite apenas um dos números citados: ");
+            break;
+    }
 }
+
+
+
